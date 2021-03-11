cf790461b7704537e10cd616367981303478f08d
HMartins-tech <79343611+HMartins-tech@users.noreply.github.com>
2021-03-11 15:11:37 -0300

    Dois Números

---
+++ Dia22.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Dia22 {
+
+	public static void main(String[] args) {
+		Int valor1, valor2, iguais, maior;
+		Scanner imput =  new Scanner (System.in);
+		
+		System.out.println("Digite o primeiro valor: ");
+		valor1 = imput.nextInt();
+		
+		System.out.println("Digite o segundo valor: ");
+		valor2 = imput.nextInt();
+		
+		if (valor1 > valor2) {
+			System.out.println("O primeiro valor é maior que o segundo.");
+		} else {
+			if (valor1 < valor2) {
+				System.out.println("O segundo valor é maior que o primeiro.");
+			} else if (valor1 == valor2) {
+				System.out.println("Os números são iguais.");
+			}
+		}
+	}
+}
