8e80d469ef8a10bf5b2e8253684460b5beb454f0
HMartins-tech <79343611+HMartins-tech@users.noreply.github.com>
2021-03-11 14:14:41 -0300

    Estoque de Produtos

---
+++ Dia18.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Prova26 {
+
+	public static void main(String[] args) {
+		int estoqueAtual, estoqueMaximo, estoqueMinimo, quantidadeMedia;
+		Scanner imput = new Scanner (System.in);
+		
+		System.out.println("Digite a quantidade atual no estoque: ");
+		estoqueAtual = imput.nextInt();
+		
+		System.out.println("Digite a quantidade de estoque máximo: ");
+		estoqueMaximo = imput.nextInt();
+		
+		System.out.println("Digite a quantidade mínima de estoque: ");
+		estoqueMinimo = imput.nextInt();
+		
+		quantidadeMedia = ((estoqueMaximo + estoqueMinimo)/2);		
+		System.out.println("O estoque médio é de: " + quantidadeMedia);
+		
+		if (estoqueAtual >= quantidadeMedia) {
+			System.out.println("Não é necessário efetuar compra.");
+		} else if (estoqueAtual < quantidadeMedia) {
+			System.out.println("É necessário efetuar compra.");
+		}
+	}
+}
