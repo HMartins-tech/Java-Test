4a3142daf0defc28deb2500bce0315a187533ead
HMartins-tech <79343611+HMartins-tech@users.noreply.github.com>
2021-03-11 01:03:31 -0300

    Promoção de Maçãs

---
+++ Dia11.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+  public class Prova16 {
+    public static void main(String[] args) {
+      float maca, m, m2;
+
+      System.out.println("O Mercado do Tião está com promoção nas maçãs. Comprando 12 maçãs ou mais, cada maçã sai por 1 real. "
+          + "Comprando menos de 12, as maçãs continuam 1,30R$");
+      System.out.println(" ");
+
+      Scanner imput = new Scanner (System.in);
+
+      System.out.println("Quantas maçãs você comprou?");
+      maca = imput.nextFloat();
+
+      if (maca >= 12) {
+        System.out.println("Cada maçã saiu por 1 real");
+        m2 = (1*maca);
+        System.out.println(" ");
+        System.out.println("O custo total das compras das maçãs foi de: " + m2 + "R$");
+      } else {
+        System.out.println("Cada maçã continuou com preço de 1,30 R$");
+        m = (float) (1.3*maca);
+        System.out.println(" ");
+        System.out.println("O custo total das compras das maçãs foi de: " + m + "R$");
+      }	
+	}
+}
