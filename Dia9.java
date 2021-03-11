e0bec51921827158a00c09632b49beac2d78f992
HMartins-tech <79343611+HMartins-tech@users.noreply.github.com>
2021-03-11 01:00:49 -0300

    Maior ou Menor

---
+++ Dia9.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Dia9 {
+	public static void main(String[] args) {
+		float valor;
+		
+		Scanner imput = new Scanner (System.in);
+		
+		System.out.println("Digite qualquer valor: ");
+		valor = imput.nextFloat();
+		
+		if (valor>10) {
+			System.out.println("O valor é maior que 10.");
+		} else {
+			System.out.println("O valor é menor que 10.");
+		}
+	}
+}
