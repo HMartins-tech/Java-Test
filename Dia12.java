a94b003464a453de2b57f4f78ce80b25df647eec
HMartins-tech <79343611+HMartins-tech@users.noreply.github.com>
2021-03-11 01:26:29 -0300

    Média de Notas

--- Dia12.java
+++ Dia12.java
@@ -2,28 +2,28 @@ import java.util.Scanner;
 
 	public class Dia12 {	
 		public static void main(String[] args) {
-			float n1, n2, n3, n4, soma, media;
+			float n1, n2, n3, n4, soma, mediaFinal;
 			Scanner imput = new Scanner (System.in);
 			
-			System.out.println("Entre com o primeiro valor: ");
+			System.out.println("Entre com a nota do 1° Bimestre: ");
 			n1 = imput.nextInt();
 			
-			System.out.println("Entre com o segundo valor: ");
+			System.out.println("Entre com a nota do 2° Bimestre: ");
 			n2 = imput.nextInt();
 			
-			System.out.println("Entre com o terceiro valor: ");
+			System.out.println("Entre com a nota do 3° Bimestre: ");
 			n3 = imput.nextInt();
 			
-			System.out.println("Entre com o quarto valor: ");
+			System.out.println("Entre com a nota do 4° Bimestre: ");
 			n4 = imput.nextInt();
 			
 			soma = (n1 + n2 + n3 + n4);
-			media = (soma/4);
+			mediaFinal = (soma/4);
 			
-			if (media >= 6) {
-				System.out.println("Você foi APROVADO! Sua nota foi: " + media);
+			if (mediaFinal >= 6) {
+				System.out.println("Você foi APROVADO! Sua nota foi: " + mediaFinal);
 			} else {
-				System.out.println("Você foi REPROVADO! Sua nota foi: " + media);
+				System.out.println("Você foi REPROVADO! Sua nota foi: " + mediaFinal);
 			}
 	}
 }
