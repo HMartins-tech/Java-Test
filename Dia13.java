e4474fb651a008b68aa25e1870c4263bc795386f
HMartins-tech <79343611+HMartins-tech@users.noreply.github.com>
2021-03-11 11:45:22 -0300

    Ano de Eleições

---
+++ Dia13.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Prova18 {
+
+	public static void main(String[] args) {
+		int anoA, anoN, conta, idade;
+		Scanner imput = new Scanner (System.in);
+		
+		System.out.println("Digite o ano atual: ");
+		anoA = imput.nextInt();
+		
+		System.out.println("Digite o ano que você nasceu: ");
+		anoN = imput.nextInt();
+		
+		conta = (anoA - anoN);
+		idade = conta;
+		
+		System.out.println("Sua idade é: " + idade);
+		
+		if (idade >= 16) {
+			System.out.println("Você já pode votar!");
+		} else {
+			System.out.println("Você não pode votar esse ano!");
+		}
+	}
+}
