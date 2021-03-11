53c75a95d999323808696f0b5a8457a45aae6a99
HMartins-tech <79343611+HMartins-tech@users.noreply.github.com>
2021-03-11 00:56:18 -0300

    Graus Fahrenheit para Celsius

---
+++ Dia7.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Dia7 {
+
+	public static void main(String[] args) {
+		int C, F;
+		
+		Scanner imput = new Scanner (System.in);
+		
+		System.out.println("Digite o valor dos graus em Fahrenheit: ");
+		F = imput.nextInt();
+		
+		C = ( F-32)* 5/9;
+		System.out.println("Os graus em Celsius são de aproximadamente: " + C + "°");	
+	}
+}
