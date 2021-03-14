a60c24a3d7ab9bf0e4a931c8aec683ebd472a131
HMartins-tech <79343611+HMartins-tech@users.noreply.github.com>
2021-03-14 17:42:44 -0300

    Desconto de produtos

---
+++ Dia24.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Prova40 {
+    @SuppressWarnings("unused")
+    public static void main(String[] args) {
+        String nome;
+        float adquirida, unico, total, totalPagar, desconto;
+        @SuppressWarnings("resource")
+        Scanner imput = new Scanner(System.in);
+
+        System.out.println("Digite o nome do produto: ");
+        nome = imput.nextLine();
+
+        System.out.println("Digite a quantidade adquirida: ");
+        adquirida = imput.nextFloat();
+
+        System.out.println("Digite o valor da peça única: ");
+        unico = imput.nextFloat();
+
+        total = (adquirida * unico);
+
+        if (adquirida >= 10) {
+            desconto = (float) (unico * 0.1);
+            totalPagar = (total - (desconto * adquirida));
+            System.out.println("Você obteve um desconto de 10%, e pagará: R$" + totalPagar);
+        } else {
+            if (adquirida > 5) {
+                desconto = (float) (unico * 0.05);
+                totalPagar = (total - (desconto * adquirida));
+                System.out.println("Você obteve um desconto de 5%, e pagará: R$" + totalPagar);
+            } else if (adquirida <= 5) {
+                desconto = (float) (unico * 0.03);
+                totalPagar = (total - (desconto * adquirida));
+                System.out.println("Você obteve um desconto de 3%, e pagará: R$" + totalPagar);
+            }
+        }
+    }
+}
