d2403c4103b1303e611d432a83b42cf1969fda6f
HMartins-tech <79343611+HMartins-tech@users.noreply.github.com>
2021-03-15 14:33:48 -0300

    Eleitores

--- Dias/Dia3.java
+++ Dias/Dia3.java
@@ -1,34 +1,35 @@
+
 import java.util.Scanner;
 
 public class Dia3 {
+	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 		float eleitores, brancos, nulos, validos, somaBrancos, somaNulos, somaValidos;
-		
-		Scanner imput = new Scanner (System.in);
-		
+
+		Scanner imput = new Scanner(System.in);
+
 		System.out.println("Digite o número total de eleitores do município: ");
 		eleitores = imput.nextFloat();
-		
+
 		System.out.println("Digite o número total de votos em BRANCO: ");
 		brancos = imput.nextFloat();
-		
+
 		System.out.println("Digite o número total de votos NULOS: ");
 		nulos = imput.nextFloat();
-		
+
 		System.out.println("Digite o número total de votos VÁLIDOS: ");
 		validos = imput.nextFloat();
-		
-		System.out.println("Os votos em branco foram de: " + brancos);		
-		somaBrancos = ((100*brancos)/eleitores);
+
+		System.out.println("Os votos em branco foram de: " + brancos);
+		somaBrancos = ((100 * brancos) / eleitores);
 		System.out.println("Em porcentagem, os votos em branco foram de: " + somaBrancos + "%");
-		
-		
-		System.out.println("Os votos anulados foram de: " + nulos);		
-		somaNulos = ((100*nulos)/eleitores);
+
+		System.out.println("Os votos anulados foram de: " + nulos);
+		somaNulos = ((100 * nulos) / eleitores);
 		System.out.println("Em porcentagem, os votos anulados foram de: " + somaNulos + "%");
-		
+
 		System.out.println("Os votos válidos foram: " + validos);
-		somaValidos = ((100*validos)/eleitores);
-		System.out.println("Em porcentagem, os votos vaválidos foram de: " + somaValidos + "%");		
+		somaValidos = ((100 * validos) / eleitores);
+		System.out.println("Em porcentagem, os votos válidos foram de: " + somaValidos + "%");
 	}
 }
