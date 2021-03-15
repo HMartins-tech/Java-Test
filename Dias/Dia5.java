7394d110cb3175218e702dd69c511f9e58474796
HMartins-tech <79343611+HMartins-tech@users.noreply.github.com>
2021-03-15 14:36:26 -0300

    Valor do Carro

--- Dias/Dia5.java
+++ Dias/Dia5.java
@@ -1,19 +1,20 @@
+
 import java.util.Scanner;
 
 public class Dia5 {
-
+	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 		int valorCarro, impostosGerais, valorFinal;
-		
-		Scanner imput = new Scanner (System.in);
-		
+
+		Scanner imput = new Scanner(System.in);
+
 		System.out.println("Digite o valor do carro sem impostos: ");
 		valorCarro = imput.nextInt();
-		
+
 		System.out.println("Agora, digite o valor, em porcentagem, dos impostos gerais: ");
 		impostosGerais = imput.nextInt();
-		
-		valorFinal = ((100*valorCarro)/impostosGerais);
-		System.out.println("O valor do seu carro com os impostos à custo de fábrica é de: " + valorFinal + "R$");
+
+		valorFinal = ((100 * valorCarro) / impostosGerais);
+		System.out.println("O valor do seu carro com os impostos e custo de fábrica é de: " + valorFinal + "R$");
 	}
 }
