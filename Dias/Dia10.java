d00d4f3ddbcf93c26249b6f2376770240e9fb9ff
HMartins-tech <79343611+HMartins-tech@users.noreply.github.com>
2021-03-15 14:49:37 -0300

    Positivo ou Negativo

---
+++ Dias/Dia10.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class Dia10 {
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		float valor;
+
+		Scanner imput = new Scanner(System.in);
+
+		System.out.println("Digite qualquer valor: ");
+		valor = imput.nextFloat();
+
+		if (valor >= 0) {
+			System.out.println("O valor é positivo.");
+		} else {
+			System.out.println("O valor é negativo.");
+		}
+	}
+}
