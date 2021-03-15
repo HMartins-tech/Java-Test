6f8cab8c8e5031e370627b8ca58277e8fe6de10b
HMartins-tech <79343611+HMartins-tech@users.noreply.github.com>
2021-03-14 22:51:52 -0300

    N° entre 10 e 1

---
+++ Dia27.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Prova58 {
+    @SuppressWarnings("resource")
+    public static void main(String[] args) {
+        int numero;
+        Scanner imput = new Scanner(System.in);
+
+        System.out.println("Aperte qualquer número para ver os valores de 10 a 1: ");
+        numero = imput.nextInt();
+
+        while (numero > -999) {
+            System.out.println("Os números entre de 10 a 1 são: " + 10);
+            System.out.println("Os números entre de 10 a 1 são: " + 9);
+            System.out.println("Os números entre de 10 a 1 são: " + 8);
+            System.out.println("Os números entre de 10 a 1 são: " + 7);
+            System.out.println("Os números entre de 10 a 1 são: " + 6);
+            System.out.println("Os números entre de 10 a 1 são: " + 5);
+            System.out.println("Os números entre de 10 a 1 são: " + 4);
+            System.out.println("Os números entre de 10 a 1 são: " + 3);
+            System.out.println("Os números entre de 10 a 1 são: " + 2);
+            System.out.println("Os números entre de 10 a 1 são: " + 1);
+            return;
+        }
+    }
+}
