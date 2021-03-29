899f96110d7344fd07adc5e7016ede9e8e55c9e5
HMartins-tech <79343611+HMartins-tech@users.noreply.github.com>
2021-03-29 17:14:59 -0300

    Salário

--- Dias/Dia04.java
+++ Dias/Dia04.java
@@ -4,19 +4,18 @@ import java.util.Scanner;
 public class Dia4 {
 	@SuppressWarnings("resource")
 	public static void main(String[] args) {
-		int salario, salarioNovo, reajuste;
-
+		float salario, salarioNovo, percentual, reajuste;
+		
 		Scanner imput = new Scanner(System.in);
-
-		System.out.println("O valor em porcentagem do reajuste salarial é de 75%");
 
 		System.out.println("Entre com o valor salarial mensal: ");
 		salario = imput.nextInt();
 
 		System.out.println("Entre com o porcentual do reajuste salarial: ");
-		reajuste = imput.nextInt();
+		percentual = imput.nextInt();
 
-		salarioNovo = ((salario*reajuste)/100);
+		reajuste = ((percentual*salario)/100);
+		salarioNovo = (salario+reajuste);
 		System.out.println("Seu novo salario mensal será de: " + salarioNovo + "R$");
 	}
 }
