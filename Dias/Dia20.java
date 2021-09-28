cea3f5f3aff5faeb66d1a4b5f6456fff0ce7c15c
HMartins-tech <79343611+HMartins-tech@users.noreply.github.com>
2021-09-28 12:46:40 -0300

    Dia20.java

--- Dias/Dia20.java
+++ Dias/Dia20.java
@@ -1,5 +1,35 @@
-//Prova29
+import java.util.Scanner;
 
-Disponibilizado futuramente
+public class Dia20 {
 
-Necessário: melhorar código
+	public static void main(String[] args) {
+		int valor1, valor2, valor3, soma;
+
+		@SuppressWarnings("resource")
+		Scanner imput = new Scanner(System.in);
+
+		System.out.println("Entre com o primeiro valor: ");
+		valor1 = imput.nextInt();
+
+		System.out.println("Entre com o segundo valor: ");
+		valor2 = imput.nextInt();
+
+		System.out.println("Entre com o terceiro valor: ");
+		valor3 = imput.nextInt();
+
+		if (valor1 > valor2) {
+			if (valor2 > valor3) {
+				soma = (valor1 + valor2);
+			} else {
+				soma = (valor1 + valor3);
+			}
+		} else {
+			if (valor1 > valor3) {
+				soma = (valor2 + valor1);
+			} else {
+				soma = (valor3 + valor2);
+			}
+		}
+		System.out.println("A soma entre os maiores numeros é: " + soma);
+	}
+}
